e5b1c012ab52f5e74c41b501e7bef1d85e5ee25f
eles linux <eleswastaken@gmail.com>
2021-12-30 07:35:58 +0600

    Wire custom button dropdown

--- dropdowns/script.js
+++ dropdowns/script.js
@@ -1,3 +1,35 @@
 
 
-console.log("hello, world!")
+{
+const btn1 = document.querySelector('#dropdown1 > button')
+
+
+btn1.addEventListener('click', function(event) {
+    // this.getAttribute('aria-expanded') === 'true'
+    //     ? this.setAttribute('aria-expanded', false)
+    //     : this.setAttribute('aria-expanded', true)
+
+    function clickAway(event) {
+        if (event.target.nodeName !== "BUTTON" & event.target !== btn1) {
+            event.preventDefault()
+            btn1.setAttribute('aria-expanded', false)
+            document.removeEventListener('click', clickAway)
+        }
+    }
+
+    if (this.getAttribute('aria-expanded') === 'true') {
+        this.setAttribute('aria-expanded', false)
+    } else {
+        this.setAttribute('aria-expanded', true)
+        document.addEventListener('click', clickAway)
+    }
+})
+
+document.querySelectorAll('#dropdown1 li button').forEach(btn => {
+    btn.addEventListener('click', function(event) {
+        btn1.textContent = btn.textContent;
+        btn1.setAttribute('aria-expanded', false)
+    })
+})
+
+}
