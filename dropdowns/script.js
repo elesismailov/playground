7b84a70479995f0c0ad8f9fe8f4d0f6e605fd93c
eles linux <eleswastaken@gmail.com>
2021-12-31 14:33:43 +0600

    Wire closing animation

--- dropdowns/script.js
+++ dropdowns/script.js
@@ -3,22 +3,35 @@
 {
 const btn1 = document.querySelector('#dropdown1 > button')
 
+function resetDropdown() {
+    function reset() {
+        btn1.nextElementSibling.removeEventListener('animationend', reset)
+        btn1.nextElementSibling.style.animationDirection = '';
+        btn1.setAttribute('aria-expanded', false)
+        // btn1.parentElement.style.pointerEvents = '';
+    }
+    setTimeout(() => {
+        btn1.nextElementSibling.style.animation = '';
+        btn1.nextElementSibling.style.animationDirection = 'reverse';
+        btn1.nextElementSibling.addEventListener('animationend', reset)
+    }, 0);
+    btn1.nextElementSibling.style.animation = 'none';
+    // btn1.parentElement.style.pointerEvents = 'none';
+}
 
 btn1.addEventListener('click', function(event) {
-    // this.getAttribute('aria-expanded') === 'true'
-    //     ? this.setAttribute('aria-expanded', false)
-    //     : this.setAttribute('aria-expanded', true)
 
     function clickAway(event) {
         if (event.target.nodeName !== "BUTTON" & event.target !== btn1) {
             event.preventDefault()
-            btn1.setAttribute('aria-expanded', false)
+            // btn1.setAttribute('aria-expanded', false)
             document.removeEventListener('click', clickAway)
+            resetDropdown()
         }
     }
 
     if (this.getAttribute('aria-expanded') === 'true') {
-        this.setAttribute('aria-expanded', false)
+        resetDropdown()
     } else {
         this.setAttribute('aria-expanded', true)
         document.addEventListener('click', clickAway)
@@ -28,8 +41,13 @@ btn1.addEventListener('click', function(event) {
 document.querySelectorAll('#dropdown1 li button').forEach(btn => {
     btn.addEventListener('click', function(event) {
         btn1.textContent = btn.textContent;
-        btn1.setAttribute('aria-expanded', false)
+        resetDropdown()
     })
+})
+
+btn1.nextElementSibling.addEventListener('animationend', function(event) {
+    console.log('ended')
+    // btn1.nextElementSibling.style.animation = '';
 })
 
 }
